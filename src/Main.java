7314d2e082f88a2bddf529374b01ad2a45070b10
My PC <alex.petkov606@gmail.com>
2022-11-19 15:34:03 +0200

    sd

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         //Enter problem:
 
-        problem1(100);
+        problem2(12);
     }
 
     public static void problem1(int age) {
@@ -18,5 +18,22 @@ public class Main {
         } else if(66 <= age) {
             System.out.println("elder");
         }
+    }
+
+    public static void problem2(int number) {
+        if (number % 10 == 0) {
+            System.out.println("The number is divisible by 10");
+        } else if (number % 7 == 0) {
+            System.out.println("The number is divisible by 7");
+        } else if (number % 6 == 0) {
+            System.out.println("The number is divisible by 6");
+        } else if(number % 3 == 0) {
+            System.out.println("The number is divisible by 3");
+        } else if (number % 2 == 0) {
+            System.out.println("The number is divisible by 2");
+        } else {
+            System.out.println("Not divisible");
+        }
+
     }
 }
